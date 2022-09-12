f69aed2f243cfcfa3fc9715d931dbef7c60ba06f
kim-seoyoung <52961246+kim-seoyoung@users.noreply.github.com>
2022-09-12 16:43:18 +0900

    Time: 246 ms (22.82%), Space: 18.5 MB (15.98%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -13,17 +13,15 @@ class Solution:
         q = []
         curr_lists = lists.copy()
         for i in range(len_):
-            if curr_lists[i] is not None:
-                heappush(q, (curr_lists[i].val, i))
+            while curr_lists[i] is not None:
+                heappush(q, curr_lists[i].val)
+                curr_lists[i] = curr_lists[i].next
         
         curr_out = output
         while len(q) > 0:
-            value, idx = heappop(q)
+            value = heappop(q)
             curr_out.next = ListNode(val=value)
-            curr_lists[idx] = curr_lists[idx].next
             curr_out = curr_out.next
-            if curr_lists[idx] is not None:
-                heappush(q, (curr_lists[idx].val, idx))
                 
         output = output.next
             
