b84f00a92d453c8232d8a55c21a4792ae4eeea58
kim-seoyoung <52961246+kim-seoyoung@users.noreply.github.com>
2022-09-12 16:38:44 +0900

    Time: 288 ms (17.06%), Space: 18.7 MB (12.51%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -4,26 +4,26 @@
 #         self.val = val
 #         self.next = next
 
-from queue import PriorityQueue
+from heapq import heappush, heappop
 
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         output = ListNode()
         len_ = len(lists)
-        q = PriorityQueue()
+        q = []
         curr_lists = lists.copy()
         for i in range(len_):
             if curr_lists[i] is not None:
-                q.put((curr_lists[i].val, i))
+                heappush(q, (curr_lists[i].val, i))
         
         curr_out = output
-        while not q.empty():
-            value, idx = q.get()
+        while len(q) > 0:
+            value, idx = heappop(q)
             curr_out.next = ListNode(val=value)
             curr_lists[idx] = curr_lists[idx].next
             curr_out = curr_out.next
             if curr_lists[idx] is not None:
-                q.put((curr_lists[idx].val, idx))
+                heappush(q, (curr_lists[idx].val, idx))
                 
         output = output.next
             
